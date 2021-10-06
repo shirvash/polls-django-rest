fbbee12fa0ed87f2520de08b4221c4be93b4a98a
shirvani <shirvash96@gmail.com>
2021-10-07 02:00:57 +0300

    add reply/ in urls

--- pollsite/polls/urls.py
+++ pollsite/polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 from django.conf.urls import url, include
 
-from polls.views import PollViewSet, QuestionViewSet
+from polls.views import PollViewSet, QuestionViewSet, ReplyViewSet
 
 router = SimpleRouter()
 router.register('polls', PollViewSet)
 router.register('questions', QuestionViewSet)
+router.register('reply', ReplyViewSet)
 
 urlpatterns = [
 
