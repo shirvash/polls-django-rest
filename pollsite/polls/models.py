4be4c0217cff7d17693c76444a98e7beeb107f97
shirvani <shirvash96@gmail.com>
2021-10-08 02:12:00 +0300

    fix bugs

--- pollsite/polls/models.py
+++ pollsite/polls/models.py
@@ -32,26 +32,29 @@ class Question(models.Model):
         )
 
     poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE, null=False, blank=False)
-    text = models.CharField(max_length=256)
+    question_text = models.CharField(max_length=256, blank=True)
     type = models.CharField(max_length=11, choices=Type.choices, default=Type.TEXT)
 
     def __str__(self):
-        return self.text
+        return self.question_text
 
 
 class Choice(models.Model):
-    text = models.CharField(max_length=255)
+    choice_text = models.CharField(max_length=255, blank=True)
     question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE, null=False, blank=False)
     poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE, null=False, blank=False)
 
     def __str__(self):
-        return self.text
+        return str(self.choice_text)
 
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=False)
     date_reply = models.DateTimeField(auto_now_add=True, editable=False, null=False)
+
+    def __str__(self):
+        return f'{self.user}. {self.poll}'
 
 
 class Answer(models.Model):
@@ -63,5 +66,5 @@ class Answer(models.Model):
     def __str__(self):
         if self.text:
             return self.text
-        elif self.choice:
-            return self.choice
+        elif self.choice.choice_text:
+            return self.choice.choice_text
